3a77dea313f75d06498dbd1a709cd60cf3f6cbfd
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-02-08 18:46:35 -0500

    Make leader board order properly & event dropdown

--- components/leader-board.vue
+++ components/leader-board.vue
@@ -4,6 +4,11 @@
       <option>Men's RX</option>
       <option>Women's RX</option>
     </select>
+    <select v-model="event" @change="updateLeaderBoard()">
+      <option>Week 1</option>
+      <option>Week 2</option>
+      <option>Week 3</option>
+    </select>
     <ol>
       <li v-for="score, index in displayedScores" :set="name = Object.keys(score)[0]">
         <span>{{ name }}: {{ score[name]["Week 1"].score }} - {{ displayedScores.length - index }} Team Points</span>
@@ -15,6 +20,7 @@
 <script setup>
 // This script uses the composition API rather than the Options API.
 const division = ref("Men's RX");
+const event = ref("Week 1");
 const displayedScores = reactive([]);
 const props = defineProps({
   scores: Object,
@@ -24,11 +30,11 @@ const props = defineProps({
 function updateLeaderBoard() {
   displayedScores.length = 0;
   for (const [name, score] of Object.entries(props.scores)) {
-    if (score["Week 1"].division === division.value) {
+    if (score[event.value].division === division.value) {
       if (displayedScores.length > 0) {
         displayedScores.forEach((value, index) => {
-          if (score["Week 1"].score > value[(Object.keys(value)[0])]["Week 1"].score) {
-            displayedScores.push({ [name]: score });
+          if (score[event.value].score > value[(Object.keys(value)[0])][event.value].score) {
+            displayedScores.splice(index, 0, { [name]: score });
           } else if (index == (displayedScores.length - 1)) {
             displayedScores.push({ [name]: score });
           }
