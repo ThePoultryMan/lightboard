3f69b78ad8c69919f2024672e33275bb4a3bfc66
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-02-10 08:38:47 -0500

    Initial header

---
+++ components/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex content-center gap-5 px-3 py-2 bg-indigo-300">
+    <h1 class="text-lg">Lightboard</h1>
+    <nav class="flex justify-between w-full">
+      <div>
+        <NuxtLink to="/" class="mx-3">Home</NuxtLink>
+        <NuxtLink to="/board">Board</NuxtLink>
+      </div>
+      <NuxtLink to="/admin">Admin</NuxtLink>
+    </nav>
+  </div>
+</template>
