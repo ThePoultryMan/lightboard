dbbbb3748c8ab7069002d93900771fe92aea1ae3
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-02-11 01:16:58 -0500

    Add footer

---
+++ components/footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center absolute right-0 bottom-0 left-0 p-3 bg-gray-300">
+    <p>Made with <Icon name="ph:heart-straight-fill" class="text-red-400" /> and spite by ThePoultryMan.</p>
+    <p>Find more of my work on <NuxtLink class="text-blue-500 underline" to="https://github.com/ThePoultryMan/">GitHub</NuxtLink></p>
+  </div>
+</template>
