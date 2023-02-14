b6108fe1e8093a5e8dd9c379a1a491c0328d3491
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-02-13 20:55:55 -0500

    Header and footer changes

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="text-center p-3 bg-gray-300">
-    <p>Made with <Icon name="ph:heart-straight-fill" class="text-red-400" /> and spite by ThePoultryMan.</p>
+  <footer class="text-center p-3 bg-gray-300">
+    <p>Made with <Icon name="ph:heart-straight-fill" class="text-red-400" /> <s>and stack overflow</s> by ThePoultryMan.</p>
     <p>Find more of my work on <NuxtLink class="text-blue-500 underline" to="https://github.com/ThePoultryMan/">GitHub</NuxtLink></p>
-  </div>
+    <div>
+      <p><NuxtLink to="/special-thanks" class="text-blue-500 underline">A special thank you</NuxtLink> to all of the open source libraries that made this website possible.</p>
+    </div>
+  </footer>
 </template>
--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center gap-5 px-3 py-2 bg-indigo-300">
+  <header class="flex items-center gap-5 px-3 py-2 bg-indigo-300">
     <h1 class="text-lg">Lightboard</h1>
     <nav class="flex justify-between w-full">
       <div>
         <NuxtLink to="/" class="mx-3">Home</NuxtLink>
-        <NuxtLink to="/board">Leader Board</NuxtLink>
+        <NuxtLink to="/board" class="mx-3">Leader Board</NuxtLink>
       </div>
-      <NuxtLink to="/admin">Admin</NuxtLink>
+      <NuxtLink to="/admin" class="mx-3">Admin</NuxtLink>
     </nav>
-  </div>
+  </header>
 </template>
