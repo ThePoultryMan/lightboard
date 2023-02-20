bb038332216c06e25af225afa81ff964307a8848
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-02-20 14:05:36 -0500

    Leader board bonus point display

--- components/leader-board.vue
+++ components/leader-board.vue
@@ -10,14 +10,14 @@
     </select>
     <hr />
     <ol class="list-decimal list-inside mt-1">
-      <li v-for="score in displayedScores.val">
-        <span>{{ score.name }}: {{ score.displayScore }} <span class="mx-3">-</span> {{ teamPoints[score.name] }} Team Points</span>
+      <li v-for="score in displayedScores.value">
+        <span>{{ score.name }}: {{ score.displayScore }} <span class="mx-3">-</span> {{ teamPoints[score.name] }} Team Points {{ getBonusPointDisplay(score.bonus) }} </span>
       </li>
     </ol>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { doc, getDoc, getFirestore } from "@firebase/firestore"
 import { getDivisions } from '~~/assets/event';
 import { generateLeaderBoard, getTeamPoints } from '~~/assets/util';
@@ -25,9 +25,9 @@ import { generateLeaderBoard, getTeamPoints } from '~~/assets/util';
 // This script uses the composition API rather than the Options API.
 const division = ref("Men's RX");
 const event = ref("Week 1");
-const displayedScores = reactive({ val: 0 });
-const scoringData = reactive((await getDoc(doc(getFirestore(), "athletes/scoring-data"))).data());
-var teamPoints = reactive({});
+const displayedScores: any = reactive({ value: {} });
+const scoringData = reactive((await getDoc(doc(getFirestore(), "athletes/scoring-data"))).data()!);
+var teamPoints: any = reactive({});
 
 const props = defineProps({
   scores: Object,
@@ -38,7 +38,24 @@ onMounted(() => {
 })
 
 function updateLeaderBoard() {
-  displayedScores.val = generateLeaderBoard(props.scores, division.value, event.value);
-  teamPoints = getTeamPoints(displayedScores.val, scoringData[division.value]);
+  displayedScores.value = generateLeaderBoard(props.scores, division.value, event.value);
+  teamPoints = getTeamPoints(displayedScores.value, scoringData[division.value]);
+}
+
+function getBonusPointDisplay(bonusPoints: number): string {
+  switch (bonusPoints) {
+    case 0:
+      return "";
+    case 1:
+      return "(+1 Bonus Point)";
+    case -1:
+      return "(-1 Bonus Point)";
+    default:
+      if (bonusPoints >= 2) {
+        return `(+${bonusPoints} Bonus Points)`;
+      } else {
+        return `(${bonusPoints} Bonus Points)`;
+      }
+  }
 }
 </script>
