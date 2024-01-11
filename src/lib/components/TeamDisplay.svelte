f4248aac538eba8474d778770bee34b8c7bbc9c6
ThePoultryMan <kellingcolehi@gmail.com>
2024-01-11 20:37:39 +0000

    style: Add flex-1 as default TeamDisplay style

--- src/lib/components/TeamDisplay.svelte
+++ src/lib/components/TeamDisplay.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  export let flexOne = true;
   export let name;
 
   let points = 0;
 </script>
 
-<div class="bg-blue-300 p-2 mb-3 rounded-lg">
+<div class="bg-blue-300 p-2 mb-3 rounded-lg" class:flex-1={flexOne}>
   <div class="flex justify-between">
     <span>{name}</span>
     <div class="text-right">
