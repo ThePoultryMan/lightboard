d5f08241b36f8beb2b85ec2f268620d038828727
ThePoultryMan <kellingcolehi@gmail.com>
2024-01-11 19:53:16 +0000

    feat: Initial header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,3 @@
+<header>
+  <span>Lightboard</span>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-	import '../app.pcss';
+  import '../app.pcss';
+
+  import Header from "$components/Header.svelte";
 </script>
 
+<Header />
 <slot />
