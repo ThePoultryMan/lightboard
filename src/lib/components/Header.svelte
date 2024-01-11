1c61b2f1a88f86dca1944498ec29a827f086dd27
ThePoultryMan <kellingcolehi@gmail.com>
2024-01-11 20:15:48 +0000

    feat: Make title a link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="sticky top-0 flex items-center gap-5 px-3 py-2 bg-indigo-300 drop-shadow">
-  <span class="text-lg">Lightboard</span>
+  <a href="/"><span class="text-lg">Lightboard</span></a>
   <nav class="flex justify-between w-full">
     <div class="[&>a]:mx-3">
       <a href="/">Home</a>
