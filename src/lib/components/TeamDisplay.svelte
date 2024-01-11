533a9c3f9ca51dff840c748226e14b07025b218c
ThePoultryMan <kellingcolehi@gmail.com>
2024-01-11 20:35:40 +0000

    feat: Add leader board page and TeamDisplay

---
+++ src/lib/components/TeamDisplay.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let name;
+
+  let points = 0;
+</script>
+
+<div class="bg-blue-300 p-2 mb-3 rounded-lg">
+  <div class="flex justify-between">
+    <span>{name}</span>
+    <div class="text-right">
+      <p class="text-lg font-semibold">{points}</p>
+      <p class="text-sm">Team Points</p>
+    </div>
+  </div>
+</div>
---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TeamDisplay from "$components/TeamDisplay.svelte";
+</script>
+
+<div class="flex m-3">
+  <TeamDisplay name="team name that is a little long" />
+</div>
