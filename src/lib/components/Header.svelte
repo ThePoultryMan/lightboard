3cb619e457a8cad91d3d3ebdb76092afe57f5e9f
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-02-08 14:30:08 -0500

    feat: link component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,7 @@
-<div class="sticky top-0 flex items-center px-3 py-2 text-lg bg-secondary-500 text-slate-200">
-  <span>Lightboard</span>
+<script>
+  import Link from "$components/Link.svelte";
+</script>
+
+<div class="bg-secondary-500 sticky top-0 flex items-center px-3 py-2 text-lg text-slate-200">
+  <span><Link href="/">Lightboard</Link></span>
 </div>
---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  let { children, href, target }: { children: Snippet; href: string; target?: string } = $props();
+</script>
+
+<a
+  {href}
+  rel={"noopener" + (target === "_blank" ? " noreferrer" : "")}
+  target={target ? target : undefined}
+>
+  {@render children()}
+</a>
