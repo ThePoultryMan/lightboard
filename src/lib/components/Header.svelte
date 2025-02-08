a40881de0a54553134b0730944c4d59900813d89
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-02-08 14:44:19 -0500

    feat: nav

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,6 +2,16 @@
   import Link from "$components/Link.svelte";
 </script>
 
-<div class="bg-secondary-500 sticky top-0 flex items-center px-3 py-2 text-lg text-slate-200">
-  <span><Link href="/">Lightboard</Link></span>
+<div class="bg-secondary-500 sticky top-0 flex items-center gap-5 px-3 py-2 text-slate-200">
+  <span class="text-lg"><Link href="/">Lightboard</Link></span>
+  <nav class="flex justify-between">
+    <div class="*:mx-3">
+      <Link href="/">
+        Home
+      </Link>
+      <Link href="/leaderboard">
+        Leaderboard
+      </Link>
+    </div>
+  </nav>
 </div>
