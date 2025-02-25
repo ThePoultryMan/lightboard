c6e59bd1cb630596be488f09a2a6045655ce7b37
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-02-25 14:44:30 -0500

    feat: account button text

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,20 +1,23 @@
 <script>
   import Link from "$components/Link.svelte";
+  import { sessionData } from "$lib/index.svelte";
 </script>
 
 <div class="bg-secondary-500 sticky top-0 flex items-center gap-5 px-3 py-2">
   <span class="text-lg"><Link href="/">Lightboard</Link></span>
-  <nav class="flex justify-between w-full">
+  <nav class="flex w-full justify-between">
     <div class="*:mx-3">
-      <Link href="/">
-        Home
-      </Link>
-      <Link href="/leaderboard">
-        Leaderboard
-      </Link>
+      <Link href="/">Home</Link>
+      <Link href="/leaderboard">Leaderboard</Link>
     </div>
     <div>
-      <Link href="/account">Sign In</Link>
+      <Link href="/account">
+        {#if sessionData.user}
+          Account
+        {:else}
+          Sign In
+        {/if}
+      </Link>
     </div>
   </nav>
 </div>
