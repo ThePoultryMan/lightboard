1916de6edb1f43e6c4591e66d97c84409dfe0127
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-03-03 15:54:20 -0500

    refactor: new leaderboard page

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,181 +1,20 @@
 <script lang="ts">
-  import type { Event, Participant } from "$lib";
-  import { getEventData } from "$lib/firebase";
-  import { sessionData, type EventCode } from "$lib/state.js";
-  import { getSortableScores, getTeamScores } from "$lib/scoring/util";
-  import { participantsIncludes } from "$lib/util";
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+  import { sessionData, type EventCode } from "$lib/state";
 
-  let eventCode: EventCode | undefined = $state();
+  let eventCode: EventCode | undefined;
   sessionData.subscribe((data) => {
-    eventCode = data.eventCode;
-  });
-  let data: Event | undefined = $state();
-  let promisedData = $derived.by(() => {
-    if (eventCode) {
-      return getEventData(eventCode.org, eventCode.event);
-    } else {
-      return getEventData("example", "example-event-1");
+    if (browser) {
+      if (data.eventCode) {
+        eventCode = data.eventCode;
+      } else {
+        eventCode = {
+          org: "example",
+          event: "example-event-1",
+        };
+      }
+      goto(`/${eventCode.org}/${eventCode.event}`);
     }
   });
-  $effect(() => {
-    promisedData.then((value) => {
-      data = value;
-    });
-  });
-  let mergedParticipants = $derived.by(() => {
-    if (!data) return [];
-    const participants: Participant[] = [];
-    data?.teams.forEach((team) => {
-      if (team.participants) {
-        participants.push(...team.participants);
-      }
-    });
-    return participants;
-  });
-  let division = $state(0);
-  let section = $state(0);
-  let summedSectionScores = $derived.by(() => {
-    if (!data?.metaData.sections || !data.metaData.divisions) return {};
-    const summedSectionScores: any = {};
-    data?.teams.forEach((team) => {
-      for (const section of data?.metaData.sections!) {
-        if (team.participants) {
-          let sum = 0;
-          for (const division of data?.metaData.divisions!) {
-            getTeamScores(
-              getSortableScores(mergedParticipants, section.index, division),
-              division,
-            ).forEach((teamScore) => {
-              if (participantsIncludes(team.participants, teamScore)) {
-                sum += teamScore.adjustedScore + teamScore.bonusPoints;
-              }
-            });
-          }
-          summedSectionScores[team.meta.displayName] = summedSectionScores[team.meta.displayName]
-            ? summedSectionScores[team.meta.displayName]
-            : {};
-          summedSectionScores[team.meta.displayName][section.index] = sum;
-        } else {
-          summedSectionScores[team.meta.displayName] = {};
-          summedSectionScores[team.meta.displayName][section.index] = 0;
-        }
-      }
-    });
-    return summedSectionScores;
-  });
-  const teamPoints: Record<string, Number> = $derived.by(() => {
-    if (summedSectionScores) {
-      const result: Record<string, number> = {};
-      for (const team of Object.keys(summedSectionScores)) {
-        let sum = 0;
-        for (const section of Object.keys(summedSectionScores[team])) {
-          sum += summedSectionScores[team][section];
-        }
-        result[team] = sum;
-      }
-      // console.log(Object.keys(summedSectionScores))
-      return result;
-    } else {
-      return {};
-    }
-  });
-  let teamDisplay: HTMLElement | undefined = $state();
-  const isScrollable = $derived(
-    teamDisplay ? teamDisplay.scrollWidth > teamDisplay.clientWidth : false,
-  );
 </script>
-
-{#if data}
-  <h1 class="mb-3.5 text-2xl font-semibold">{data.metaData.displayName}</h1>
-  <!--Team Display-->
-  <div class={`mb-4 sm:flex sm:flex-wrap sm:justify-center md:gap-3`}>
-    {#each data.teams as team}
-      <div
-        class={`mb-2.5 rounded-lg bg-red-900 p-2 max-md:last:mb-4 md:mb-0 ${data.teams.length >= 5 ? "md:w-<-1/3" : "md:w-<-1/2"}`}
-      >
-        <!--Team Header-->
-        <div class="mb-3 flex justify-between">
-          <p class="text-xl">{team.meta.displayName}</p>
-          <div class="text-right">
-            <p class="m-1 text-lg leading-2 font-semibold">
-              {teamPoints ? teamPoints[team.meta.displayName] : 0}
-            </p>
-            <p class="mr-1 text-sm">Team Points</p>
-          </div>
-        </div>
-        <!--Section Display-->
-        <div
-          class={"flex h-44 items-end gap-4 overflow-x-scroll rounded-lg border-1 border-red-500 p-2.5 pb-0 " +
-            (isScrollable ? "justify-normal" : "justify-between")}
-        >
-          {#each data.metaData.sections as section, index}
-            <!--We have to use this twice so we only bind the first display-->
-            {#if index === 0}
-              <div class="flex w-28 shrink-0 gap-3" bind:this={teamDisplay}>
-                <div class="text-right">
-                  <p>{section.displayName}</p>
-                  <p class="font-bold">
-                    {summedSectionScores[team.meta.displayName][section.index]
-                      ? summedSectionScores[team.meta.displayName][section.index]
-                      : 0}
-                  </p>
-                </div>
-                <div class="w-5 shrink-0 rounded-t-md bg-red-500" style="height: 48px;"></div>
-              </div>
-            {:else}
-              <div class="flex w-28 shrink-0 gap-3">
-                <div class="text-right">
-                  <p>{section.displayName}</p>
-                  <p class="font-bold">
-                    {summedSectionScores[team.meta.displayName][section.index]
-                      ? summedSectionScores[team.meta.displayName][section.index]
-                      : 0}
-                  </p>
-                </div>
-                <div class="w-5 shrink-0 rounded-t-md bg-red-500" style="height: 48px;"></div>
-              </div>
-            {/if}
-          {/each}
-        </div>
-      </div>
-    {/each}
-  </div>
-  <!--Leader Board-->
-  <div class="rounded-lg bg-indigo-900 p-3">
-    <div class="mb-1.5 flex gap-3">
-      <h2 class="text-lg">Leaderboard</h2>
-      <select bind:value={section} class="rounded-md bg-indigo-950 px-1.5 py-1">
-        {#each data.metaData.sections as section}
-          <option value={section.index}>{section.displayName}</option>
-        {/each}
-      </select>
-      <select bind:value={division} class="rounded-md bg-indigo-950 px-1.5 py-1">
-        {#each data.metaData.divisions as division}
-          <option value={division.index}>{division.displayName}</option>
-        {/each}
-      </select>
-    </div>
-    <ol class="list-inside list-decimal">
-      {#each getTeamScores(getSortableScores(mergedParticipants, section, data.metaData.divisions[division]), data.metaData.divisions[division]) as teamScore}
-        <li>
-          {teamScore.name}: {teamScore.score} <span class="mx-3">-</span>
-          {teamScore.adjustedScore} Team Points {teamScore.bonusPoints
-            ? `(+${teamScore.bonusPoints} Bonus point${teamScore.bonusPoints > 1 ? "s" : ""})`
-            : ""}
-        </li>
-      {/each}
-    </ol>
-  </div>
-{/if}
-
-<style>
-  @media (width >= 48rem) {
-    .md\:w-\<-1\/2 {
-      width: calc(50% - 6px);
-    }
-
-    .md\:-w-1\<-\/3 {
-      width: calc(33% - 6px);
-    }
-  }
-</style>
